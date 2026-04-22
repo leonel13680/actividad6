d5178b157a11d71511238fda4bd663d3b34cd8f1
christopher <48525330@fatimarem.edu.ar>
2026-04-22 13:49:48 -0300

    corrijo ejercicio

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Estudiantes estudiante = new Estudiantes ("Miguel","Villarrubia",22,"holasebas@gmail.com","6666-6666","villa fatima","no se que es legajo",666999,(float) 9.9,2000,"fisica cauntica",10);
+		Estudiantes estudiante = new Estudiantes ("Leandro","Rodriguez",17,"leandrorodriguez@gmail.com","3042-7239","villa soldati","EST2024",123545,(float) 10.0,3000,"educacion fisica",2);
 		
 		estudiante.mostrarDatos();
 		
